22b05eee88ed9c57f6b57ae475867f014e36ce43
Venkata Bhaskararao Madasu <mvbvenkat@gmail.com>
2025-08-13 10:48:42 -0500

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello the World!";
+    private static final String MESSAGE = "Hello the into World!";
 
     public App() {}
 
